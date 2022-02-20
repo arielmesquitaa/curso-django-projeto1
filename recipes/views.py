484c07a8ffaf8ba23605666a94aa4f406a804cc8
Ariel Mesquita <arielmesquita@live.com>
2022-02-19 21:22:48 -0300

    Recipes - views and urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request: HttpRequest):
+    return HttpResponse('HOME!')
+
+
+def sobre(request: HttpRequest):
+    return HttpResponse('SOBRE!')
+
+
+def contato(request: HttpRequest):
+    return HttpResponse('CONTATO!')
