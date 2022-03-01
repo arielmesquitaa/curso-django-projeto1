997c4de56540799d63595aa8cbf8b93906c84fff
Ariel Mesquita <arielmesquita@live.com>
2022-03-01 20:49:34 -0300

    Refactoring

--- project/settings.py
+++ project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['core'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 
 # Register your models here.
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
-admin.site.register(Category, CategoryAdmin)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
